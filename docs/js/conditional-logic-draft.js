2e64422b989323e37a2af3a26c20347d3cb4d133
willcapellaro <will@capellaro.com>
2021-10-08 08:30:23 -0500

    quadrant label fix

--- docs/js/conditional-logic-draft.js
+++ docs/js/conditional-logic-draft.js
@@ -7,7 +7,7 @@ function conditionals(x, y)
 {
 	x = x.toLowerCase();
 	y = y.toLowerCase();
-	var texts = [('high ' + y + ' and low ' + x), ('high ' + y + ' and high ' + x), ('low ' + y + ' and high ' + x), ('low ' + y + ' and low ' + x)];
+	var texts = [('high ' + y + ' and low ' + x), ('high ' + y + ' and high ' + x), ('low ' + y + ' and low ' + x), ('low ' + y + ' and high ' + x)];
 	
 	texts = getTexts(x, y, texts, ['urgency', 'importance'], ['schedule', 'focus', 'avoid', 'delegate']);
 	/*In this case when x = 'urgency' and y = 'importance', then the texts will be located as follows:
@@ -23,7 +23,7 @@ function conditionals(x, y)
 		'schedule'//bottomright
 	In other words, it will go from '0, 1, 2, 3' to '3, 1, 2, 0'.*/
 	
-	texts = getTexts(x, y, texts, ['importance', 'ease'], ['luxury', 'strategic', 'distractions', 'high value']);
+	texts = getTexts(x, y, texts, ['priority', 'ease'], ['luxury', 'strategic', 'distractions', 'high value']);
 	
 	texts = getTexts(x, y, texts, ['low balance', 'high interest'], ['pay off first', 'pay down first', 'luxury', 'high value']);
 	
